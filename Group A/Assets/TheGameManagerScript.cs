a72840164339dc93b2997c4eaa1ccdf127798c17
slitthegoblin <73849505+slitthegoblin@users.noreply.github.com>
2021-03-25 01:06:54 +0000

    inventoryUI fixed
    i fix inventyory UI and the spikey asset prefab and  all teh items spawn now and hte UI inventory is fixed burron function working - code isnt added yet for this i am doing now again - i deleted all my codes sorry x

--- Group A/Assets/TheGameManagerScript.cs
+++ Group A/Assets/TheGameManagerScript.cs
@@ -9,12 +9,15 @@ public class TheGameManagerScript : MonoBehaviour
     public GameObject axePrefab;
     public GameObject knifeWeaponSpawn;
     public GameObject knifeWeaponPrefab;
+    public GameObject spikedWeaponSpawn;
+    public GameObject spikedWeaponPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(axePrefab, axeSpawn.transform.position, Quaternion.identity);
         Instantiate(knifeWeaponPrefab, knifeWeaponSpawn.transform.position, Quaternion.identity);
+        Instantiate(spikedWeaponPrefab, spikedWeaponSpawn.transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
