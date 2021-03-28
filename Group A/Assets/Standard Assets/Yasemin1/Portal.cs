a4c4de84b5524edb8542361bfbd719bbd7c8f862
slitthegoblin <73849505+slitthegoblin@users.noreply.github.com>
2021-03-28 17:30:06 +0100

    fixed the textueres
    fixed textures
    yas

---
+++ Group A/Assets/Standard Assets/Yasemin1/Portal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.position = new Vector3(other.transform.position.x +10, other.transform.position.y + 10, other.transform.position.z);
+            //setting pos of character to new set of coordinates - in that new set of coord u are taking current position then adding 10 to Y so he falls from y axis in air
+        }
+    }
+}
