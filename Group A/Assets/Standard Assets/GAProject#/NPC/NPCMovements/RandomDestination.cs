5c33c06508ed94a086fc1ab6b5167ba627d1df4a
lolnevermore <tasasr@gmail.com>
2021-04-05 16:26:19 +0100

    better shit

---
+++ Group A/Assets/Standard Assets/GAProject#/NPC/NPCMovements/RandomDestination.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDestination : MonoBehaviour { 
+
+    public int xPos;
+    public int zPos;
+
+    void OnTriggerEnter(Collider something)
+{
+        if (something.tag == "NPC")
+        {
+            xPos = Random.Range(150, 100);
+            zPos = Random.Range(150, 100);
+            this.gameObject.transform.position = new Vector3(xPos, 0.5f, zPos);
+        }
+}
+
+
+
+}
---
+++ Group A/Assets/Standard Assets/GAProject#/StaminaBar/PlayerStamina.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RpgAdventure
+{
+    public class PlayerStamina : MonoBehaviour
+    {
+        //    public Rigidbody rb;
+        public float staminaAmount = 100f;
+        Animator animator;
+        InventoryManager inventoryManager;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            staminaAmount = 100f;
+            inventoryManager = GetComponent<InventoryManager>();
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (animator.GetBool("isRunning"))
+            {
+                //StaminaBar.instance.staminaBar.value = StaminaBar.instance.staminaBar.value;
+                int totalWeight = inventoryManager.GetTotalweight();
+                staminaAmount -= 0.2f * totalWeight;
+                if (staminaAmount < 0) staminaAmount = 0;
+                if (staminaAmount == 0) animator.SetBool("isRunning", false);
+            }
+            else
+            {
+                staminaAmount += 0.1f;
+                if (staminaAmount > 100) staminaAmount = 100;
+            }
+            StaminaBar.instance.staminaBar.value = staminaAmount;
+        }
+
+    }
+}
+
