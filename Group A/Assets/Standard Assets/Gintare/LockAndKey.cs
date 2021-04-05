33377e530cd73f1d9ae454bb57e8ebeb1f2afef6
lolnevermore <tasasr@gmail.com>
2021-04-05 16:09:30 +0100

    Revert "345534"
    This reverts commit c0631a69493035b3ebc7de6cf0684caedef54369.

--- Group A/Assets/Standard Assets/Gintare/LockAndKey.cs
+++ Group A/Assets/Standard Assets/Gintare/LockAndKey.cs
@@ -4,41 +4,41 @@ using UnityEngine;
 
 public class LockAndKey : MonoBehaviour
 {
-    //[SerializeField] public bool locked = false;
-    //[SerializeField] GameObject requierment1;
-    // [SerializeField] private Host questClass;
-    GameObject questItem;
+//    //[SerializeField] public bool locked = false;
+//    //[SerializeField] GameObject requierment1;
+//   // [SerializeField] private Host questClass; //change the serialzie feild and maybe not monobehaviour change to the inventoryslot - or link to there
+//    GameObject questItem;
+    
+//    List<Item> inventory = new List<Item>();
 
-    List<Item> inventory = new List<Item>();
+//    public void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if(collision.gameObject.tag == "Player")
+//        {
+//          //check the global inventory and maek sure it contains hte object "vile 1, tag the viles as seperate things
+//          //if vile 1 is == true then need to set the questClass to true 
+//        }
+//    }
+//    public void OnCollisionEnter(Collision collision)
+//    {
+//        if(collision.gameObject.tag == "Vile") // if requierment1 is in the inventory
+//        {
+// //           if(collision.gameObject.tag ("Vile") == questItem) // if requierment1 i sthe right item
+//            {
+// //               questClass.playerCanTalkToNpcNr1 = true; // set qest bool to true
+//            }
+//        }
+//    }
 
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            //check the global inventory and maek sure it contains hte object "vile 1, tag the viles as seperate things
-            //if vile 1 is == true then need to set the questClass to true 
-        }
-    }
-    public void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Vile") // if requierment1 is in the inventory
-        {
-            //           if(collision.gameObject.tag ("Vile") == questItem) // if requierment1 i sthe right item
-            {
-                //               questClass.playerCanTalkToNpcNr1 = true; // set qest bool to true
-            }
-        }
-    }
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+      
+//    }
 }
