2bb1466f7f91aa3f8b11f71948203d4bb82d0499
GinSad <gs5299@greenwich.ac.uk>
2021-04-07 12:15:51 +0300

    CameraForVirginia

---
+++ Group A/Assets/Standard Assets/Gintare/move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float speed = 9.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal, 0, vertical) * (speed * Time.deltaTime));
+
+        if (characterController.isGrounded)
+        {
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
