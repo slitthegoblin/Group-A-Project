363df6e136cec1aa46445a08efac72d0c4638aa8
GinSad <gs5299@greenwich.ac.uk>
2021-04-08 04:03:00 +0300

    wokrs or no?

--- Group A/Assets/Standard Assets/Gintare/LockAndKey.cs
+++ Group A/Assets/Standard Assets/Gintare/LockAndKey.cs
@@ -6,31 +6,22 @@ namespace RpgAdventure
 {
     public class LockAndKey : MonoBehaviour
     {
-        //[SerializeField] public bool locked = false;
-        //[SerializeField] GameObject requierment1;
         [SerializeField] public Host host;
         
         GameObject questItem;
 
-        List<Item> inventory = new List<Item>();
+       // List<Item> inventory = new List<Item>();
 
-        public void OnTriggerEnter2D(Collider2D collision)
-        {
-            if (collision.gameObject.tag == "Player")
-            {
-                //check the global inventory and maek sure it contains hte object "vile 1, tag the viles as seperate things
-                //if vile 1 is == true then need to set the questClass to true 
-            }
-        }
+
         public void OnCollisionEnter(Collision collision)
         {
             if (collision.gameObject.tag == "Vile") // if requierment1 is in the inventory
             {
-                //           if(collision.gameObject.tag ("Vile") == questItem) // if requierment1 i sthe right item
-                {
-                    //               questClass.playerCanTalkToNpcNr1 = true; // set qest bool to true
-                }
+            
+                    host.playerCanTalkToNpcNr1 = true; // set qest bool to true
+             
             }
+
         }
     }
 }
