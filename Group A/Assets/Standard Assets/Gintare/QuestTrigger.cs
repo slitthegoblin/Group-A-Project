93615490a10031f600b07958e00c530c90b8a507
GinSad <gs5299@greenwich.ac.uk>
2021-04-08 05:36:11 +0300

    Quest UI trigger Code

---
+++ Group A/Assets/Standard Assets/Gintare/QuestTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestTrigger : MonoBehaviour
+{
+    public Image image;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (other.gameObject.tag == "QuestT1")
+            {
+                image.transform.localScale = new Vector3(1, 1, 1);
+                Destroy(gameObject);
+            }
+            else if (other.gameObject.tag == "QuestT2")
+            {
+                image.transform.localScale = new Vector3(1, 1, 1);
+                Destroy(gameObject);
+            }
+            else if (other.gameObject.tag == "QuestT3")
+            {
+                image.transform.localScale = new Vector3(1, 1, 1);
+                Destroy(gameObject);
+            }
+            else if (other.gameObject.tag == "QuestT4")
+            {
+                image.transform.localScale = new Vector3(1, 1, 1);
+                Destroy(gameObject);
+            }
+            else if (other.gameObject.tag == "QuestT5")
+            {
+                image.transform.localScale = new Vector3(1, 1, 1);
+                Destroy(gameObject);
+            }
+            else if (other.gameObject.tag == "QuestT6")
+            {
+                image.transform.localScale = new Vector3(1, 1, 1);
+                Destroy(gameObject);
+            }
+            else if (other.gameObject.tag == "QuestT7")
+            {
+                image.transform.localScale = new Vector3(1, 1, 1);
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+}
